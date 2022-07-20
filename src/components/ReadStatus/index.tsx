23ce84ec0320762d23ef2718bd03be9113b52525
Mansehej Singh <mansehej@gmail.com>
2022-07-20 18:57:14 +0530

    feat[dev]: Add custom component to mark as read

---
+++ src/components/ReadStatus/index.tsx
@@ -0,0 +1,35 @@
+import { localStorageService } from "@site/src/services/storage/localStorage";
+import React from "react";
+
+interface IReadStatusProps {
+  frontMatter: any;
+}
+
+const ReadStatus = (props: IReadStatusProps): JSX.Element => {
+  const lessons = (props.frontMatter.lessons ?? [])
+    .split(",")
+    .map((lesson: string) => lesson.trim());
+
+  const completionStatus = localStorageService.areLessonsComplete(lessons);
+
+  const [read, setRead] = React.useState(completionStatus);
+
+  const handleChange = () => {
+    const newReadStatus = !read;
+    if (newReadStatus) {
+      localStorageService.updateCompletedLessons(lessons);
+    } else {
+      localStorageService.removeCompletedLessons(lessons);
+    }
+    setRead(newReadStatus);
+  };
+
+  return (
+    <div>
+      Hello
+      <input type="checkbox" checked={read} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default ReadStatus;

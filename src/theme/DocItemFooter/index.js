1658a0f1e18c1ce2ec5c188ca21d8650a3f5accc
Mansehej Singh <mansehej@gmail.com>
2022-07-23 12:15:15 +0530

    fix[dev]: Remove console log

--- src/theme/DocItemFooter/index.js
+++ src/theme/DocItemFooter/index.js
@@ -3,7 +3,6 @@ import DocItemFooter from "@theme-original/DocItemFooter";
 import ReadStatus from "@site/src/components/ReadStatus";
 
 export default function DocItemFooterWrapper(props) {
-  console.log("props", props);
   return (
     <>
       <ReadStatus frontMatter={props.content.frontMatter} />
